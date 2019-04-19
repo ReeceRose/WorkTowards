034abf46f7f5183470b522aeb02c56378b069d35
reecerose <reecerose18@gmail.com>
2019-04-19 13:25:17 -0400

    Added app bar

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -4,19 +4,28 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text(
+          'Work Towards',
+          style: TextStyle(
+            color: Colors.black,
+            fontFamily: 'Bungee',
+          ),
+        ),
+        backgroundColor: Colors.white,
+        actions: <Widget>[
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.add),
+            color: Colors.black,
+          ),
+        ],
+      ),
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.center,
-        children: <Widget>[
-          Container(
-            height: 250.0,
-            alignment: Alignment(0, 1),
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage('assets/images/logo.png'),
-              ),
-            ),
-          )
-        ],
+        children: <Widget>[Container()],
       ),
     );
   }
