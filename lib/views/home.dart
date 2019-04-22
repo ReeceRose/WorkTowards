5f42ca24ec2138598b4db150fe6b7e7dbf138b2a
reecerose <reecerose18@gmail.com>
2019-04-22 01:12:16 -0400

    Moved quick add to a seperate container and not center of page

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -16,6 +16,7 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
+        elevation: 0.0,
         centerTitle: true,
         title: Text(
           'Work Towards',
@@ -25,109 +26,106 @@ class _HomePageState extends State<HomePage> {
           ),
         ),
         backgroundColor: Colors.white,
-        actions: <Widget>[
-          IconButton(
-            onPressed: () {
-              Navigator.pushNamed(context, '/settings');
-            },
-            icon: Icon(Icons.settings),
-            color: Colors.black,
-          ),
-        ],
       ),
-      body: Center(
-        child: Container(
-          width: inputWidth,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              SizedBox(
-                height: 15.0,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Container(
+            alignment: Alignment(0, 0),
+            child: Container(
+              width: inputWidth,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20.0),
+                color: Colors.grey.shade200,
               ),
-              Container(
-                child: TextField(
-                  decoration: InputDecoration(
-                    labelStyle: TextStyle(fontFamily: 'Bungee'),
-                    labelText: 'Enter Price',
-                    fillColor: Colors.white,
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(25.0),
-                      borderSide: BorderSide(),
-                    ),
-                  ),
-                  onChanged: (value) {
-                    setState(
-                      () {
-                        _itemPrice = double.parse(value);
-                      },
-                    );
-                    _calculateTax();
-                  },
-                  textAlign: TextAlign.center,
-                  keyboardType: TextInputType.number,
-                ),
-              ),
-              Visibility(
-                visible: _includeTax,
+              child: Padding(
+                padding: EdgeInsets.fromLTRB(15.0, 15.0, 15.0, 15.0),
                 child: Column(
                   children: <Widget>[
-                    SizedBox(
-                      height: 15.0,
-                    ),
-                    Container(
-                      child: TextField(
-                        decoration: InputDecoration(
-                          labelStyle: TextStyle(fontFamily: 'Bungee'),
-                          labelText: 'Enter Tax Rate',
-                          fillColor: Colors.white,
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(25.0),
-                            borderSide: BorderSide(),
-                          ),
+                    TextField(
+                      decoration: InputDecoration(
+                        labelStyle: TextStyle(fontFamily: 'Bungee'),
+                        labelText: 'Enter Price',
+                        fillColor: Colors.white,
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(25.0),
+                          borderSide: BorderSide(),
                         ),
-                        onChanged: (value) {
-                          setState(
-                            () {
-                              // this will give us a number like 1.XX
-                              _taxRate = (double.parse(value) / 100) + 1;
-                            },
-                          );
-                          _calculateTax();
-                        },
-                        textAlign: TextAlign.center,
-                        keyboardType: TextInputType.number,
                       ),
-                    ),
-                  ],
-                ),
-              ),
-              Container(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    Text('Include Tax?'),
-                    Switch(
-                      value: _includeTax,
                       onChanged: (value) {
                         setState(
                           () {
-                            _includeTax = value;
+                            _itemPrice = double.parse(value);
                           },
                         );
                         _calculateTax();
                       },
-                      activeColor: Colors.blue,
+                      textAlign: TextAlign.center,
+                      keyboardType: TextInputType.number,
+                    ),
+                    Visibility(
+                      visible: _includeTax,
+                      child: Column(
+                        children: <Widget>[
+                          SizedBox(
+                            height: 10.0,
+                          ),
+                          TextField(
+                            decoration: InputDecoration(
+                              labelStyle: TextStyle(fontFamily: 'Bungee'),
+                              labelText: 'Enter Tax Rate',
+                              fillColor: Colors.white,
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(25.0),
+                                borderSide: BorderSide(),
+                              ),
+                            ),
+                            onChanged: (value) {
+                              setState(
+                                () {
+                                  // this will give us a number like 1.XX
+                                  _taxRate = (double.parse(value) / 100) + 1;
+                                },
+                              );
+                              _calculateTax();
+                            },
+                            textAlign: TextAlign.center,
+                            keyboardType: TextInputType.number,
+                          ),
+                        ],
+                      ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Text('Include Tax?'),
+                        Switch(
+                          value: _includeTax,
+                          onChanged: (value) {
+                            setState(
+                              () {
+                                _includeTax = value;
+                              },
+                            );
+                            _calculateTax();
+                          },
+                          activeColor: Colors.blue,
+                        ),
+                      ],
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Text('Total'),
+                        Text("\$$_calculatedPrice")
+                      ],
                     ),
                   ],
                 ),
               ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[Text('Total'), Text("\$$_calculatedPrice")],
-              )
-            ],
+            ),
           ),
-        ),
+        ],
       ),
     );
   }
@@ -135,7 +133,6 @@ class _HomePageState extends State<HomePage> {
   void _calculateTax() {
     setState(
       () {
-        print(_itemPrice);
         _calculatedPrice =
             (_itemPrice * (_includeTax ? _taxRate : 1)).roundToDouble();
       },
