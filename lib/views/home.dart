2fefa05fa162e356a94c0d7afdeb62417cfc20a6
reecerose <reecerose18@gmail.com>
2019-04-21 23:52:17 -0400

    Added _calculateTax method

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -6,7 +6,10 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  bool includeTax = true;
+  bool _includeTax = true;
+  double _itemPrice = 0.0;
+  double _taxRate = 1.13;
+  double _calculatedPrice = 0.0;
   @override
   Widget build(BuildContext context) {
     double inputWidth = MediaQuery.of(context).size.width / 1.25;
@@ -33,87 +36,111 @@ class _HomePageState extends State<HomePage> {
         ],
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            SizedBox(
-              height: 15.0,
-            ),
-            Container(
-              width: inputWidth,
-              child: TextFormField(
-                decoration: InputDecoration(
-                  labelStyle: TextStyle(fontFamily: 'Bungee'),
-                  labelText: 'Enter Price',
-                  fillColor: Colors.white,
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(25.0),
-                    borderSide: BorderSide(),
-                  ),
-                ),
-                validator: (val) {
-                  if (val.length == 0) {
-                    return "Please enter a valid number";
-                  } else {
-                    return null;
-                  }
-                },
-                keyboardType: TextInputType.number,
+        child: Container(
+          width: inputWidth,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              SizedBox(
+                height: 15.0,
               ),
-            ),
-            Visibility(
-              visible: includeTax,
-              child: Column(
-                children: <Widget>[
-                  SizedBox(
-                    height: 15.0,
-                  ),
-                  Container(
-                    width: inputWidth,
-                    child: TextFormField(
-                      decoration: InputDecoration(
-                        labelStyle: TextStyle(fontFamily: 'Bungee'),
-                        labelText: 'Enter Tax Rate',
-                        fillColor: Colors.white,
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(25.0),
-                          borderSide: BorderSide(),
-                        ),
-                      ),
-                      validator: (val) {
-                        if (val.length == 0) {
-                          return "Please enter a valid tax rate";
-                        } else {
-                          return null;
-                        }
-                      },
-                      keyboardType: TextInputType.number,
+              Container(
+                width: inputWidth,
+                child: TextField(
+                  decoration: InputDecoration(
+                    labelStyle: TextStyle(fontFamily: 'Bungee'),
+                    labelText: 'Enter Price',
+                    fillColor: Colors.white,
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(25.0),
+                      borderSide: BorderSide(),
                     ),
                   ),
-                ],
+                  onChanged: (value) {
+                    setState(
+                      () {
+                        _itemPrice = double.parse(value);
+                      },
+                    );
+                    _calculateTax();
+                  },
+                  textAlign: TextAlign.center,
+                  keyboardType: TextInputType.number,
+                ),
               ),
-            ),
-            Container(
-              width: inputWidth,
-              child: Row(
+              Visibility(
+                visible: _includeTax,
+                child: Column(
+                  children: <Widget>[
+                    SizedBox(
+                      height: 15.0,
+                    ),
+                    Container(
+                      width: inputWidth,
+                      child: TextField(
+                        decoration: InputDecoration(
+                          labelStyle: TextStyle(fontFamily: 'Bungee'),
+                          labelText: 'Enter Tax Rate',
+                          fillColor: Colors.white,
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(25.0),
+                            borderSide: BorderSide(),
+                          ),
+                        ),
+                        onChanged: (value) {
+                          setState(
+                            () {
+                              // this will give us a number like 1.XX
+                              _taxRate = (double.parse(value) / 100) + 1;
+                            },
+                          );
+                          _calculateTax();
+                        },
+                        textAlign: TextAlign.center,
+                        keyboardType: TextInputType.number,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                width: inputWidth,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text('Include Tax?'),
+                    Switch(
+                      value: _includeTax,
+                      onChanged: (value) {
+                        setState(
+                          () {
+                            _includeTax = value;
+                          },
+                        );
+                        _calculateTax();
+                      },
+                      activeColor: Colors.blue,
+                    ),
+                  ],
+                ),
+              ),
+              Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  Text('Include Tax?'),
-                  Switch(
-                    value: includeTax,
-                    onChanged: (value) {
-                      setState(() {
-                        includeTax = value;
-                      });
-                    },
-                    activeColor: Colors.blue,
-                  ),
-                ],
-              ),
-            )
-          ],
+                children: <Widget>[Text('Total'), Text("\$$_calculatedPrice")],
+              )
+            ],
+          ),
         ),
       ),
+    );
+  }
+
+  void _calculateTax() {
+    setState(
+      () {
+        print(_itemPrice);
+        _calculatedPrice = (_itemPrice * (_includeTax ? _taxRate : 1)).roundToDouble();
+      },
     );
   }
 }
