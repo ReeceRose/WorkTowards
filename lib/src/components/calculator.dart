fbe993246420df27b11e06c826e03a511b3eb701
reecerose <reecerose18@gmail.com>
2019-05-07 20:34:26 -0400

    Added padding on add page so it is equal on both pages

--- lib/src/components/calculator.dart
+++ lib/src/components/calculator.dart
@@ -35,109 +35,113 @@ class Calculator extends StatelessWidget {
   }
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.fromLTRB(15.0, 15.0, 15.0, 15.0),
-      child: Column(
-        children: <Widget>[
-          // Note: this text field will be auto populated by the hourly rate set in the settings
-          TextField(
-            decoration: InputDecoration(
-              labelText: 'Hourly Rate',
-              hintText: '20',
-              fillColor: Colors.white,
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(25.0),
-                borderSide: BorderSide(),
+    return Container(
+      padding: EdgeInsets.fromLTRB(20.0, 0.0, 20.0, 0.0),
+      child: Padding(
+        padding: EdgeInsets.fromLTRB(15.0, 15.0, 15.0, 15.0),
+        child: Column(
+          children: <Widget>[
+            // Note: this text field will be auto populated by the hourly rate set in the settings
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'Hourly Rate',
+                hintText: '20',
+                fillColor: Colors.white,
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(25.0),
+                  borderSide: BorderSide(),
+                ),
               ),
+              controller: TextEditingController(text: '20'),
+              enabled: false,
+              textAlign: TextAlign.center,
+              keyboardType: TextInputType.number,
             ),
-            controller: TextEditingController(text: '20'),
-            enabled: false,
-            textAlign: TextAlign.center,
-            keyboardType: TextInputType.number,
-          ),
-          SizedBox(
-            height: 10,
-          ),
-          StreamBuilder(
-            stream: _calculatorBloc.priceStream$,
-            builder: (BuildContext context, AsyncSnapshot snap) {
-              return NumberInput(
-                label: 'Enter Price',
-                hint: '200',
-                controller: _priceController,
-              );
-            },
-          ),
-          StreamBuilder(
-            stream: _calculatorBloc.includeTaxStream$,
-            builder: (BuildContext context, AsyncSnapshot snap) {
-              return Visibility(
-                visible: _calculatorBloc.includeTax,
-                child: Column(
+            SizedBox(
+              height: 10,
+            ),
+            StreamBuilder(
+              stream: _calculatorBloc.priceStream$,
+              builder: (BuildContext context, AsyncSnapshot snap) {
+                return NumberInput(
+                  label: 'Enter Price',
+                  hint: '200',
+                  controller: _priceController,
+                );
+              },
+            ),
+            StreamBuilder(
+              stream: _calculatorBloc.includeTaxStream$,
+              builder: (BuildContext context, AsyncSnapshot snap) {
+                return Visibility(
+                  visible: _calculatorBloc.includeTax,
+                  child: Column(
+                    children: <Widget>[
+                      SizedBox(
+                        height: 10.0,
+                      ),
+                      StreamBuilder(
+                        stream: _calculatorBloc.taxRateStream$,
+                        builder: (BuildContext context, AsyncSnapshot snap) {
+                          return NumberInput(
+                            label: 'Enter Tax Rate',
+                            hint: '13',
+                            controller: _taxRateController,
+                          );
+                        },
+                      ),
+                    ],
+                  ),
+                );
+              },
+            ),
+            StreamBuilder(
+              stream: _calculatorBloc.includeTaxStream$,
+              initialData: true,
+              builder: (BuildContext context, AsyncSnapshot snap) {
+                return Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
                   children: <Widget>[
-                    SizedBox(
-                      height: 10.0,
-                    ),
-                    StreamBuilder(
-                      stream: _calculatorBloc.taxRateStream$,
-                      builder: (BuildContext context, AsyncSnapshot snap) {
-                        return NumberInput(
-                          label: 'Enter Tax Rate',
-                          hint: '13',
-                          controller: _taxRateController,
-                        );
+                    Text('Include Tax?'),
+                    Switch(
+                      value: snap.data,
+                      onChanged: (value) {
+                        _calculatorBloc.includeTax = value;
                       },
+                      activeColor: Colors.blue,
                     ),
                   ],
-                ),
-              );
-            },
-          ),
-          StreamBuilder(
-            stream: _calculatorBloc.includeTaxStream$,
-            initialData: true,
-            builder: (BuildContext context, AsyncSnapshot snap) {
-              return Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: <Widget>[
-                  Text('Include Tax?'),
-                  Switch(
-                    value: snap.data,
-                    onChanged: (value) {
-                      _calculatorBloc.includeTax = value;
-                    },
-                    activeColor: Colors.blue,
-                  ),
-                ],
-              );
-            },
-          ),
-          StreamBuilder(
-            stream: _calculatorBloc.calculatedPriceStream$,
-            builder: (BuildContext context, AsyncSnapshot snap) {
-              return Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  Text('Total'),
-                  Text('\$${snap.data}'),
-                ],
-              );
-            },
-          ),
-          StreamBuilder(
-            stream: _calculatorBloc.hoursNeededStream$,
-            builder: (BuildContext context, AsyncSnapshot snap) {
-              return Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  Text('Hours Needed'),
-                  Text('${snap.data}'),
-                ],
-              );
-            },
-          ),
-        ],
+                );
+              },
+            ),
+            StreamBuilder(
+              stream: _calculatorBloc.calculatedPriceStream$,
+              builder: (BuildContext context, AsyncSnapshot snap) {
+                return Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text('Total'),
+                    Text('\$${snap.data}'),
+                  ],
+                );
+              },
+            ),
+            StreamBuilder(
+              stream: _calculatorBloc.hoursNeededStream$,
+              builder: (BuildContext context, AsyncSnapshot snap) {
+                return Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text('Hours Needed'),
+                    Text('${snap.data}'),
+                  ],
+                );
+              },
+            ),
+          ],
+        ),
       ),
     );
+    // return
   }
 }
--- lib/src/views/overview.dart
+++ lib/src/views/overview.dart
@@ -5,15 +5,13 @@ import 'package:WorkTowards/src/components/calculator.dart';
 // import 'package:flutter_circular_chart/flutter_circular_chart.dart';
 
 class OverviewPage extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return ListView(
       shrinkWrap: true,
-      padding: EdgeInsets.fromLTRB(20.0, 0.0, 20.0, 0.0),
       children: <Widget>[
         Padding(
-          padding: EdgeInsets.fromLTRB(0.0, 10.0, 0.0, 10.0),
+          padding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
           child: Text(
             'Quick Calculate',
             textAlign: TextAlign.center,
